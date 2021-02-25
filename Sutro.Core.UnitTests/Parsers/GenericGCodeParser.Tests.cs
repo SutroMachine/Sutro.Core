8214b9b8db6c3c10d3c8a2829f4d96b3549bf114
Greg Meess <greg@sutromachine.com>
2021-02-24 19:12:49 -0800

    Fix handling of N codes to avoid assuming subsequent tokens

---
+++ Sutro.Core.UnitTests/Parsers/GenericGCodeParser.Tests.cs
@@ -0,0 +1,36 @@
+﻿using gs;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sutro.Core.Models.GCode;
+
+namespace Sutro.Core.UnitTests.Parsers
+{
+    [TestClass]
+    public class GenericGCodeParserTests
+    {
+        [TestMethod]
+        public void ParsesNCodeOnly()
+        {
+            var parser = new GenericGCodeParser();
+            var line = parser.ParseLine("N100", 0);
+            Assert.AreEqual(LineType.Blank, line.Type);
+        }
+
+        [TestMethod]
+        public void ParsesNCodeWithGCode()
+        {
+            var parser = new GenericGCodeParser();
+            var line = parser.ParseLine("N100 G1", 0);
+            Assert.AreEqual(LineType.GCode, line.Type);
+        }
+
+        [TestMethod]
+        [DataRow("NumberOfThings")]
+        [DataRow("Number Of Things")]
+        public void ParsesUnknownString(string s)
+        {
+            var parser = new GenericGCodeParser();
+            var line = parser.ParseLine(s, 0);
+            Assert.AreEqual(LineType.UnknownString, line.Type);
+        }
+    }
+}
