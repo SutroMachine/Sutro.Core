74ca73e4b2b66496fefb92436cd46adeef006bb9
Greg Meess <greg@sutromachine.com>
2021-07-19 09:49:34 -0700

    Add more MachineClass enum values

--- Sutro.Core/Settings/Machine/MachineClass.cs
+++ Sutro.Core/Settings/Machine/MachineClass.cs
@@ -2,8 +2,18 @@
 {
     public enum MachineClass
     {
+        // IMPORTANT: Do not change the order of existing enum values, otherwise
+        // compatability of serialized objects may be inconsistent
+
         Unknown,
         PlasticFFFPrinter,
-        MetalSLSPrinter
+        MetalSLSPrinter,
+        MetalDODPrinter,
+        MetalEBMPrinter,
+        MetalLMDPrinter,
+        MetalMPAPrinter,
+        PlasticSLAPrinter,
+        PlasticDLPPrinter,
+        PlasticSLSPrinter
     }
 }
